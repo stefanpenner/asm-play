c1db2b57de356b68fef4a36711a3a959eba5d7a5
Stefan Penner <stefan.penner@gmail.com>
2024-12-01 13:26:21 -0700

    tidyup

--- hello.s
+++ hello.s
@@ -15,12 +15,12 @@
 .endm
 
 .macro write_string std, string, len
-  // write(stdout, "Banana!\n", 14)
+  // write(stdout, "String\n", len)
   mov x0, #\std                // File descriptor: stdout (1st argument)
   adrp x1, \string@PAGE        // Load page-aligned address of the string
   add x1, x1, \string@PAGEOFF  // Add offset to get the full address (2nd argument)
   mov x2, #\len                // Length of the string (3rd argument)
-  syscall 0x4                  // call write
+  syscall 0x4                  // Call write syscall
 .endm
 
 .macro syscall num
@@ -31,28 +31,24 @@
 
 .globl _main
 _main:
-  write_string STDOUT, hello_world, 14
-  write_string STDOUT, banana, 7
+  write_string STDOUT, hello_world, 14 // Print "Hello, World!"
+  write_string STDOUT, banana, 7       // Print "Banana"
 
-  mov x0, 5 // set the loop counter to 5
-  bl loop
+  mov x0, 5             // Set the loop counter to 5
+  bl loop               // Call the loop function
 
-  // exit
-  mov x0, #EXIT_SUCCESS
-  syscall 0x1
-  ret
-
+  mov x0, #EXIT_SUCCESS // Exit code
+  syscall 0x1           // Exit syscall
 
 loop:
-  cmp x0, 0 // compare to 0
-  beq done_loop
-  sub x0, x0, 1 // decrement
-
-  write_string STDOUT, banana, 7
-  b loop
+  cmp x0, 0             // Compare counter to 0
+  beq done_loop         // If counter == 0, exit loop
+  sub x0, x0, 1         // Decrement counter
+  write_string STDOUT, banana, 7 // Print "Banana"
+  b loop                // Repeat loop
 
 done_loop:
-  ret
+  ret                   // Return to caller (_main)
 
 .section __TEXT,__cstring,cstring_literals
 hello_world:
