80b2f1499bd572e9a08bb03203cc434b1b41a27f
Stefan Penner <stefan.penner@gmail.com>
2024-12-01 14:03:37 -0700

    improve the loop more

--- hello.s
+++ hello.s
@@ -1,57 +1,93 @@
 .section __TEXT,__text,regular,pure_instructions
 .align 2
 
+// System constants
 .equ STDOUT, 1
+.equ STDERR, 2
 .equ EXIT_SUCCESS, 0
-.equ FAILURE, 1
+.equ EXIT_FAILURE, 1
 
-.macro frame
-  stp x29, x30, [sp, #-16]!
-  mov x29, sp
+// System calls
+.equ SYS_exit, 0x1
+.equ SYS_write, 0x4
+.equ SYS_CLASS, 0x2000000
+
+.macro push_pair reg1, reg2
+    stp \reg1, \reg2, [sp, #-16]!
 .endm
 
-.macro unframe
-  ldp x29, x30, [sp], #16
+.macro pop_pair reg1, reg2
+    ldp \reg1, \reg2, [sp], #16
 .endm
 
-.macro write_string std, string, len
-  // write(stdout, "String\n", len)
-  mov x0, #\std                // File descriptor: stdout (1st argument)
-  adrp x1, \string@PAGE        // Load page-aligned address of the string
-  add x1, x1, \string@PAGEOFF  // Add offset to get the full address (2nd argument)
-  mov x2, #\len                // Length of the string (3rd argument)
-  syscall 0x4                  // Call write syscall
+.macro frame_setup
+    push_pair x29, x30    // Save frame pointer and link register
+    mov x29, sp           // Set up frame pointer
+    push_pair x19, x20    // Save callee-saved registers
 .endm
 
-.macro syscall num
-  movz x16, #0x2000, lsl #16  // Load syscall base: 0x20000000
-  orr x16, x16, #\num         // Set syscall number: write (0x20000004)
-  svc #0                      // Perform the syscall
+.macro frame_teardown
+    pop_pair x19, x20      // Restore callee-saved registers
+    pop_pair x29, x30      // Restore frame pointer and link register
+.endm
+
+.macro exit code
+    mov x0, #\code         // Exit code
+    mov x16, #SYS_CLASS    // System call class
+    orr x16, x16, #SYS_exit // Exit system call
+    svc #0
+.endm
+
+.macro write fd, string, len
+    mov x0, #\fd                // File descriptor
+    adrp x1, \string@PAGE      // Load string address (page)
+    add x1, x1, \string@PAGEOFF // Add page offset
+    mov x2, #\len              // String length
+    mov x16, #SYS_CLASS        // System call class
+    orr x16, x16, #SYS_write   // Write system call
+    svc #0
+    cmn x0, #1                 // Check for error
+    b.eq write_error           // Branch if error
 .endm
 
 .globl _main
 _main:
-  write_string STDOUT, hello_world, 14 // Print "Hello, World!"
-  write_string STDOUT, banana, 7       // Print "Banana"
+    frame_setup
 
-  mov x19, 5             // Set the loop counter to 5
-  bl loop               // Call the loop function
+    write STDOUT, hello_world, 14
+    write STDOUT, banana, 7
 
-  mov x0, #EXIT_SUCCESS // Exit code
-  syscall 0x1           // Exit syscall
+    bl print_loop
 
-loop:
-  cmp x19, 0             // Compare counter to 0
-  beq done_loop         // If counter == 0, exit loop
-  sub x19, x19, 1         // Decrement counter
-  write_string STDOUT, banana, 7 // Print "Banana"
-  b loop                // Repeat loop
+    frame_teardown
+    exit EXIT_SUCCESS
 
-done_loop:
-  ret                   // Return to caller (_main)
+print_loop:
+    frame_setup
+
+    // Initialize loop counter
+    mov x19, 5
+
+1:  // Loop
+    cbz x19, 2f              // Exit if counter is zero
+    write STDOUT, banana, 7
+    sub x19, x19, #1         // Decrement counter
+    b 1b                     // Continue loop
+
+2:  // loop done
+    frame_teardown 
+    ret
+
+write_error:
+    write STDERR, error_msg, 13
+    exit EXIT_FAILURE
+
+.section __TEXT,__const
+hello_world:
+    .asciz "Hello, World!\n"
+banana:
+    .asciz "Banana\n"
+error_msg:
+    .asciz "Write error!\n"
 
 .section __TEXT,__cstring,cstring_literals
-hello_world:
-  .asciz "Hello, World!\n"
-banana:
-  .asciz "Banana\n"
